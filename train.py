b60fa80d87446cdeda661241a95f55e50aeb835a
Dan Guest <dguest@cern.ch>
2017-05-17 19:28:05 +0200

    add flatten function

--- train.py
+++ train.py
@@ -13,13 +13,20 @@ def get_args():
     parser.add_argument('input_file')
     return parser.parse_args()
 
+def flatten(ds):
+    ftype = [(n, float) for n in ds.dtype.names]
+    flat = ds.astype(ftype).view(float).reshape(ds.shape + (-1,))
+    return flat.swapaxes(1, len(ds.shape))
+
 def run():
     args = get_args()
     with h5py.File(args.input_file, 'r') as hdf_file:
-        sample_jets = hdf_file['jets'][0:10]
-        sample_tracks = hdf_file['tracks'][0:10,:]
-    print(sample_tracks.dtype.names)
-    print(sample_jets.dtype.names)
+        sample_jets = hdf_file['jets'][0:3]
+        sample_tracks = hdf_file['tracks'][0:3,:]
+
+    print(flatten(sample_jets[['pt', 'eta']]))
+    print(flatten(sample_tracks[['pt','numberOfPixelHits']]))
+
 
 if __name__ == '__main__':
     run()
